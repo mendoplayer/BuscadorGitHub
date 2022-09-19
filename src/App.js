c836f906e65c75b0aa3ba45dccf636450ae9fb09
capacitacion colsa <capacitacion@colsa.com.ar>
2022-09-18 23:31:13 -0300

    pagina 1 terminada con animacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import {motion, useAnimation} from 'framer-motion' 
+
+
+const estilo = {
+  cajaGral :{
+    backgroundColor: "#000000",
+    height: '100vh',
+  },
+  header:{
+    height: '70vh',
+  },
+titulo:{
+  padding: 20,
+  fontFamily: 'Bungee Spice', 
+  fontSize: '10vh',
+},
+input:{
+border: '1px solid',
+borderRadius: 10,
+padding:5
+},
+label:{  
+paddingLeft: 10,
+}
+}
 
 function App() {
+
+  const letrasAnimation = useAnimation();
+
+  useEffect(() => {
+   letrasAnimation.start({
+    x: 50,
+    transition: {duration: 4},     
+   });
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={estilo.cajaGral}>
+      <header className="center row"  style={estilo.header}>
+         <motion.h1 
+            className="white-text container " 
+            style={estilo.titulo}
+            animate={letrasAnimation} 
+            initial={{x: -1000}}        
+            >
+              ¡ Bienvenidos 
+              </motion.h1>
+         <motion.h2 
+         className="white-text container" 
+         style={estilo.titulo}
+         animate={letrasAnimation} 
+         initial={{x: -2000}}  
+         >
+          al buscador de
+          </motion.h2>
+         <motion.h2 
+         className="white-text container" 
+         style={estilo.titulo}
+         animate={letrasAnimation} 
+         initial={{x: -3000}}  
+         > 
+         GitHub !
+         </motion.h2>  
+     </header>
+         <body >
+         <motion.form 
+         className="container row valign-wrapper"
+         animate={letrasAnimation} 
+         initial={{x: -4000}}  
+         >   
+        <div className="input-field col s6 offset-s1" >
+          <input  id="repositorio" type="text" className=" white-text"  style={estilo.input}/>
+          <label for="repositorio " style={estilo.label}>Repositorio</label>
+        </div>             
+        <button class="btn green darken-3  waves-effect waves-light col s2 offset-s1" type="submit" name="action">Buscar</button>
+    </motion.form>
+         </body>
     </div>
   );
 }
 
 export default App;
+
